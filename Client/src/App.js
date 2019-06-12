a2dc6615341256e8d09b26fa292bd9aed395b801
Don Charest <doncharest@gmail.com>
2019-06-12 00:38:43 -0400

    updated client package

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,30 +1,24 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import { createStore } from 'redux'
-import { HashRouter} from 'react-router-dom';
-import reducer from './reducers/reducer'
-import Main from './containers/Main';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { HashRouter } from "react-router-dom";
+import reducer from "./reducers/reducer";
+import Main from "./containers/Main";
 
-import './app.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
 
 function App() {
+  const store = createStore(reducer);
 
-  const store = createStore(reducer)
-  
   return (
     <Provider store={store}>
-       <HashRouter>
-        <Main />  
+      <HashRouter>
+        <Main />
       </HashRouter>
     </Provider>
   );
 }
 
 export default App;
-
-
-
-
-
