1a3852f0b0c6f1692f30193833824aa49df51ad2
Don Charest <doncharest@gmail.com>
2019-06-12 00:17:42 -0400

    added backend and Modals

--- Client/src/containers/Main.js
+++ Client/src/containers/Main.js
@@ -1,40 +1,59 @@
-import React, {Component} from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import { Route, Link, Redirect }from 'react-router-dom';
-import SprintPage from './sprint/SprintPage'
-import BacklogPage from './backlog/BacklogPage'
-import AdminPage from './admin/AdminPage'
-import ChatPage from './chat/ChatPage'
+import React, { Component } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Route, Link, Redirect } from "react-router-dom";
+import SprintPage from "./sprint/SprintPage";
+import BacklogPage from "./backlog/BacklogPage";
+import AdminPage from "./admin/AdminPage";
+import ChatPage from "./chat/ChatPage";
+
+import LoginModal from "./auth/LoginModal";
 
 class Main extends Component {
-
   render() {
+    return (
+      <div>
+        <Container>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand>
+              <Link to="/">TracKing</Link>
+            </Navbar.Brand>
 
+            <Nav className="mr-auto">
+              <Nav.Item>
+                <Link to="/sprint">Sprint</Link>
+              </Nav.Item>
 
-  return (
-    <div>
-      <Container>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand><Link to="/">TracKing</Link></Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Item><Link to="/sprint">Sprint</Link></Nav.Item>
-            <Nav.Item><Link to="/backlog">Backlog</Link></Nav.Item>
-            <Nav.Item><Link to="/admin">Administration</Link></Nav.Item>
-            <Nav.Item><Link to="/chat">Chat</Link></Nav.Item>
-          </Nav>
-        </Navbar>
+              <Nav.Item>
+                <Link to="/backlog">Backlog</Link>
+              </Nav.Item>
 
-        <div>
-          <Route path="/sprint" exact component={SprintPage} />
-          <Route path="/backlog" exact component={BacklogPage} />
-          <Route path="/admin" exact component={AdminPage} />
-          <Route path="/chat" exact component={ChatPage} />
-          <Redirect to="/sprint" />
-        </div>
-      </Container>
+              <Nav.Item>
+                <Link to="/admin">Administration</Link>
+              </Nav.Item>
 
-    </div>
-  )
+              <Nav.Item>
+                <Link to="/chat">Chat</Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Link to="/auth">Login</Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar>
+
+          <div>
+            <Route path="/sprint" exact component={SprintPage} />
+            <Route path="/backlog" exact component={BacklogPage} />
+            <Route path="/admin" exact component={AdminPage} />
+            <Route path="/chat" exact component={ChatPage} />
+
+            <Route path="/auth" exact component={LoginModal} />
+
+            <Redirect to="/sprint" />
+          </div>
+        </Container>
+      </div>
+    );
   }
 }
 
