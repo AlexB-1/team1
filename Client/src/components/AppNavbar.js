c93f3c649a9b7ed9b1e5bfdcfbd24bd8cf886f3f
Don Charest <doncharest@gmail.com>
2019-06-17 22:17:32 -0400

    .

--- Client/src/components/AppNavbar.js
+++ Client/src/components/AppNavbar.js
@@ -75,7 +75,7 @@ class AppNavbar extends Component {
         <div>
           <Navbar color="dark" dark expand="sm" className="mb-0">
             <Link className="navbar-brand" to="/">
-              Strength Training App
+              TracKing
             </Link>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
--- Client/src/components/Home.js
+++ Client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import gymImage from "../images/15428190060701082885942.jpg";
+import crown from "../images/crown.jpeg";
 import { Container } from "reactstrap";
 import PropTypes from "prop-types";
 
@@ -12,32 +13,18 @@ const Home = props => {
   return (
     <div>
       <Container>
-        <h1>Welcome to the Strength Training App!</h1>
+        <h1>Welcome to TracKing!</h1>
         <hr />
         <p>
-          This application is designed to introduce the novice lifter to the
-          types of exercises, training programs and nutrition required to build
-          a foundation in strength and add muscle mass. &nbsp;
+          The Agile Project Management Application. &nbsp;
           <i>
             Please login or register for a free account today and begin using
-            the App to create your own personalized workout plan and track your
-            progress.
+            the App to manage your development projects.
           </i>
         </p>
         <div className="gymImage">
-          <img src={gymImage} alt="gymImage" />
+          <img src={crown} alt="crown" />
         </div>
-        <br />
-        <p>
-          <i>
-            The Content contained within this application is not intended to be
-            a substitute for professional medical advice, diagnosis, or
-            treatment. Always seek the advice of your physician or other
-            qualified health provider prior to beginning an exercise routine, or
-            with any questions you may have regarding preexisting medical
-            conditions.
-          </i>
-        </p>
       </Container>
     </div>
   );
